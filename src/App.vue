1eb0b5694214c6ee1635b2738723c66ccdfd7164
beverlyn <beverly893@gmail.com>
2022-07-14 17:34:02 -0400

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,70 @@
 <script setup>
 import ProgressBar from './components/ProgressBar.vue';
 
+function currentYearDay() {
+  const current = new Date();
+  const start = new Date(current.getFullYear(), 0, 0);
+  const diff =
+    current -
+    start +
+    (start.getTimezoneOffset() - current.getTimezoneOffset()) * 60 * 1000;
+  const oneDay = 1000 * 60 * 60 * 24;
+  const yearDay = Math.floor(diff / oneDay);
 
-export default {
-  methods: {
-    currentYearDay() {
-      const current = new Date();
-      const start = new Date(current.getFullYear(), 0, 0);
-      const diff = (current - start) + ((start.getTimezoneOffset() - current.getTimezoneOffset()) * 60 * 1000);
-      const oneDay = 1000 * 60 * 60 * 24;
-      const yearDay = Math.floor(diff / oneDay);
+  const year = current.getFullYear();
+  const daysInYear =
+    (year % 4 === 0 && year % 100 > 0) || year % 400 == 0 ? 366 : 365;
 
-      const year = current.getFullYear()
-      const daysInYear = ((year % 4 === 0 && year % 100 > 0) || year %400 == 0) ? 366 : 365;
+  const yearPerc = (yearDay / daysInYear) * 100;
+  return yearPerc;
+}
 
-      const yearPerc = (yearDay/daysInYear) * 100;
-      return yearPerc;
-    },
+function currentMonthDay() {
+  const current = new Date();
+  const currentYear = current.getFullYear();
+  const currentMonth = current.getMonth() + 1;
+  const daysInMonth = new Date(currentYear, currentMonth, 0).getDate();
 
-    currentMonthDay() {
-      const current = new Date();
-      const currentYear = date.getFullYear();
-      const currentMonth = date.getMonth() + 1;
+  const monthDay = current.getDate();
 
-      const daysInMonth = new Date(currentYear, currentMonth, 0).getDate();
+  const monthPerc = (monthDay / daysInMonth) * 100;
+  return monthPerc;
+}
 
-      const monthDay = current.getDate()
+function currentWeekday() {
+  const current = new Date();
+  const currentDay = current.getDay() + 1;
+  const weekPerc = (currentDay / 7) * 100;
+  return weekPerc;
+}
 
-      const monthPerc = (monthDay/daysInMonth) * 100
-      return monthPerc;
-    }
-  }
+function currentDayHour() {
+  const current = new Date();
+  const currentHour = current.getHours();
+  const currentMinute = current.getMinutes();
+  const totalMinute = currentHour * 60 + currentMinute;
+
+  const dayPerc = (totalMinute / 1440) * 100;
+  return dayPerc;
+}
+
+function currentHour() {
+  const current = new Date();
+  const currentMinute = current.getMinutes();
+
+  const hourPerc = (currentMinute / 60) * 100;
+  return hourPerc;
 }
 
 </script>
 
 <template>
-  <ProgressBar label="Year" maxN="365 days" perc={{ currentYearDay() }}/>
-  <ProgressBar label="Month" maxN="30 days"/>
-  <ProgressBar label="Week" maxN="Saturday"/>
-  <ProgressBar label="Day" maxN="24h"/>
-  <ProgressBar label="Hour" maxN="60m"/>
-  <ProgressBar label="Minute" maxN="60s"/>
+  <ProgressBar label="Year" maxN="365 days" :perc="currentYearDay()" />
+  <ProgressBar label="Month" maxN="30 days" :perc="currentMonthDay()" />
+  <ProgressBar label="Week" maxN="Saturday" :perc="currentWeekday()" />
+  <ProgressBar label="Day" maxN="24h" :perc="currentDayHour()"/>
+  <ProgressBar label="Hour" maxN="60m" />
+  <ProgressBar label="Minute" maxN="60s" />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
