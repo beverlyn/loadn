d51b9c86869e7b5008bb8c93641435574d6d66b9
beverlyn <beverly893@gmail.com>
2022-07-14 17:11:58 -0400

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,42 @@
 <script setup>
-import ProgressBar from './components/ProgressBar.vue'
+import ProgressBar from './components/ProgressBar.vue';
+
+
+export default {
+  methods: {
+    currentYearDay() {
+      const current = new Date();
+      const start = new Date(current.getFullYear(), 0, 0);
+      const diff = (current - start) + ((start.getTimezoneOffset() - current.getTimezoneOffset()) * 60 * 1000);
+      const oneDay = 1000 * 60 * 60 * 24;
+      const yearDay = Math.floor(diff / oneDay);
+
+      const year = current.getFullYear()
+      const daysInYear = ((year % 4 === 0 && year % 100 > 0) || year %400 == 0) ? 366 : 365;
+
+      const yearPerc = (yearDay/daysInYear) * 100;
+      return yearPerc;
+    },
+
+    currentMonthDay() {
+      const current = new Date();
+      const currentYear = date.getFullYear();
+      const currentMonth = date.getMonth() + 1;
+
+      const daysInMonth = new Date(currentYear, currentMonth, 0).getDate();
+
+      const monthDay = current.getDate()
+
+      const monthPerc = (monthDay/daysInMonth) * 100
+      return monthPerc;
+    }
+  }
+}
+
+
+
+
+
 </script>
 
 <template>
