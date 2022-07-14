dada039b4cf67161a397c1cca134f5a11ac0aa2a
beverlyn <beverly893@gmail.com>
2022-07-14 16:48:55 -0400

    2 files modified, 1 added

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,16 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import ProgressBar from './components/ProgressBar.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <ProgressBar label="Year" maxN="365 days"/>
+  <ProgressBar label="Month" maxN="30 days"/>
+  <ProgressBar label="Week" maxN="Saturday"/>
+  <ProgressBar label="Day" maxN="24h"/>
+  <ProgressBar label="Hour" maxN="60m"/>
+  <ProgressBar label="Minute" maxN="60s"/>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,45 @@
+<script setup>
+defineProps({
+  label: String,
+  maxN: String,
+});
+</script>
+
+<template>
+  <p class="label">
+    {{ label }}
+  </p>
+  <div class="progressBar">
+    <div class="progress">80%</div>
+  </div>
+  <p class="maxN">
+    {{ maxN }}
+  </p>
+</template>
+
+<style scoped>
+    .label {
+        font-size: 20px;
+        text-align: left;
+    }
+  
+    .progressBar {
+        background-color: rgb(192, 192, 192);
+        width: 100%;
+    }
+  
+    .progress {
+        background-color: rgb(116, 194, 92);
+        color: white;
+        padding: 1%;
+        text-align: right;
+        font-size: 20px;
+        width:80%;
+    }
+
+    .maxN {
+      text-align: right;
+      margin-top: 0px;
+    }
+
+</style>
