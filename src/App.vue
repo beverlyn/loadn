bed37b1df0acd725264c0f9834bd3faf260b0602
beverlyn <beverly893@gmail.com>
2022-07-14 20:45:01 -0400

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,11 @@
 import { reactive } from 'vue';
 import ProgressBar from './components/ProgressBar.vue';
 
+function getDaysInYear(date) {
+  const year = date.getFullYear();
+  return (year % 4 === 0 && year % 100 > 0) || year % 400 == 0 ? 366 : 365;
+}
+
 function getYearProgress(date) {
   const start = new Date(date.getFullYear(), 0, 0);
   const diff =
@@ -11,22 +16,14 @@ function getYearProgress(date) {
   const oneDay = 1000 * 60 * 60 * 24;
   const yearDay = Math.floor(diff / oneDay);
 
-  const year = date.getFullYear();
-  const daysInYear =
-    (year % 4 === 0 && year % 100 > 0) || year % 400 == 0 ? 366 : 365;
-
-  return yearDay / daysInYear;
+  return yearDay / getDaysInYear(date);
 }
 
-function getMonthProgress(date) {
-  const daysInMonth = new Date(
-    date.getFullYear(),
-    date.getMonth() + 1,
-    0
-  ).getDate();
-  return date.getDate() / daysInMonth;
+function getDaysInMonth(date) {
+  return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
 }
 
+const getMonthProgress = (date) => date.getDate() / getDaysInMonth(date);
 const getWeekProgress = (date) => (date.getDay() + 1) / 7;
 const getDayProgress = (date) => (date.getHours() * 60 + date.getMinutes()) / 1440;
 const getHourProgress = (date) => date.getMinutes() / 60;
@@ -40,6 +37,9 @@ const progress = reactive({
   day: getDayProgress(date),
   hour: getHourProgress(date),
   minute: getMinuteProgress(date),
+
+  totalYearDays: getDaysInYear(date),
+  totalMonthDays: getDaysInMonth(date),
 });
 
 setInterval(() => {
@@ -52,12 +52,15 @@ setInterval(() => {
   progress.day = getDayProgress(date);
   progress.hour = getHourProgress(date);
   progress.minute = getMinuteProgress(date);
+
+  progress.yearDays = getDaysInYear(date);
+  progress.monthDays = getDaysInMonth(date);
 }, 1000);
 </script>
 
 <template>
-  <ProgressBar title="Year" end-label="365 days" :percent="progress.year" />
-  <ProgressBar title="Month" end-label="30 days" :percent="progress.month" />
+  <ProgressBar title="Year" :end-label="progress.yearDays" :percent="progress.year" />
+  <ProgressBar title="Month" :end-label="progress.monthDays" :percent="progress.month" />
   <ProgressBar title="Week" end-label="Saturday" :percent="progress.week" />
   <ProgressBar title="Day" end-label="24h" :percent="progress.day" />
   <ProgressBar title="Hour" end-label="60m" :percent="progress.hour" />
