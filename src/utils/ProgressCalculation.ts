b0cc04d1f942d18fcd8471959e5702f817544841
Beverly Nguyen <beverly893@gmail.com>
2024-01-13 11:02:15 +0900

    Updates

--- src/utils/ProgressCalculation.ts
+++ src/utils/ProgressCalculation.ts
@@ -20,7 +20,7 @@ export function getYearProgress(date: Date) {
     return ((getDaysInYear(date) - getDaysLeftInYear(date)) * 24 + date.getHours()) / 24;
 }
 export const getMonthProgress = (date: Date) => ((date.getDate() - 1) * 24 + date.getHours()) / 24;
-export const getWeekProgress = (date: Date) => ((date.getDay() + 1) * 24 + date.getHours()) / 24;
+export const getWeekProgress = (date: Date) => ((date.getDay() ) * 24 + date.getHours()) / 24;
 export const getDayProgress = (date: Date) => (date.getHours() * 60 + date.getMinutes()) / 60;
 export const getHourProgress = (date: Date) => date.getMinutes();
 export const getMinuteProgress = (date: Date) => date.getSeconds();
